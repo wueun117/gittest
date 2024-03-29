11f0578b388f4ff18c288df71002b69b7a62a8bf
wueun117 <100269243+wueun117@users.noreply.github.com>
2024-03-29 15:02:40 +0800

    Add files via upload

--- CS/app_checkChain.py
+++ CS/app_checkChain.py
@@ -5,7 +5,32 @@ def generate_sha256(input_string):
     sha256_hash.update(input_string.encode('utf-8'))
     return sha256_hash.hexdigest()
 
-# 示例用法
-input_string = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9"
-sha256_result = generate_sha256(input_string)
-print("SHA-256 哈希结果:", sha256_result)
+start =  "SHARE/" + "1.txt"
+text=[1,2,3,4,5,6,7]
+while len(text) == 7 :
+    print("NOW: ",start)
+    p = open(start)
+    content = p.read()
+    sha256_result = generate_sha256(content)
+    p.close
+    p2 = open(start)
+    text = []
+    for line in p2:
+        text.append(line)
+    next = text[1].split(": ")
+    print("NEXT: ",next[1],end="")
+    start = "SHARE/" + next[1][:-1]
+    p2.close
+
+    f = open(start)
+    text = []
+    for line in f:
+        text.append(line)
+    hash=text[0].split(": ")
+    if hash[1][:-1] == sha256_result:
+        print("NO PROBLEM")
+    else:
+        print("ERROR")    
+    f.close
+
+
--- CS/app_checkLog.py
+++ CS/app_checkLog.py
@@ -1,26 +1,20 @@
-
 person=input("輸入查詢對象: ")
 start = "SHARE/" + "1.txt"
-
 text=[1,2,3,4,5,6,7]
-
 num=1
 while len(text) == 7 :
     f = open(start) 
     text = []
     for line in f:
         text.append(line)
-
     next = text[1][12:-1]
-
     for i in range(2,len(text)):
         temp = text[i].split(", ")
         if temp[0] == person:
-            print(text[i],end='')
+            print(text[i][:-1])
         elif temp[1] == person:
-            print(text[i],end='')
+            print(text[i][:-1])
     f.close
-    print('\n',end='')
     print("Block",num)
     print()
     num+=1
