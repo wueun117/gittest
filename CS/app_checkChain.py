b95e80a409291e18f5d8d27343f19dcf913407c8
wueun117 <100269243+wueun117@users.noreply.github.com>
2024-03-29 16:02:30 +0800

    Add files via upload

--- CS/app_checkChain.py
+++ CS/app_checkChain.py
@@ -5,32 +5,35 @@ def generate_sha256(input_string):
     sha256_hash.update(input_string.encode('utf-8'))
     return sha256_hash.hexdigest()
 
-start =  "SHARE/" + "1.txt"
-text=[1,2,3,4,5,6,7]
-while len(text) == 7 :
-    print("NOW: ",start)
-    p = open(start)
-    content = p.read()
-    sha256_result = generate_sha256(content)
-    p.close
-    p2 = open(start)
-    text = []
-    for line in p2:
-        text.append(line)
-    next = text[1].split(": ")
-    print("NEXT: ",next[1],end="")
-    start = "SHARE/" + next[1][:-1]
-    p2.close
+def checkChain():
+    start =  "SHARE/" + "1.txt"
+    text=[1,2,3,4,5,6,7]
+    while len(text) == 7 :
+        print("NOW: ",start)
+        p = open(start)
+        content = p.read()
+        sha256_result = generate_sha256(content)
+        p.close
+        p2 = open(start)
+        text = []
+        for line in p2:
+            text.append(line)
+        next = text[1].split(": ")
+        print("NEXT: ",next[1],end="")
+        start = "SHARE/" + next[1][:-1]
+        p2.close
 
-    f = open(start)
-    text = []
-    for line in f:
-        text.append(line)
-    hash=text[0].split(": ")
-    if hash[1][:-1] == sha256_result:
-        print("NO PROBLEM")
-    else:
-        print("ERROR")    
-    f.close
+        f = open(start)
+        text = []
+        for line in f:
+            text.append(line)
+        hash=text[0].split(": ")
+        if hash[1][:-1] == sha256_result:
+            print("NO PROBLEM")
+        else:
+            print("ERROR")    
+        f.close
+
+checkChain()
 
 
---
+++ CS/app_transaction.py
@@ -0,0 +1,39 @@
+import hashlib
+import sys
+def generate_sha256(input_string):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_string.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+trade=input("輸入交易紀錄: ")
+start = "SHARE/" + "1.txt"
+text=[1,2,3,4,5,6,7]
+
+while len(text) == 7 :
+    try:
+        p = open(start)
+        content = p.read()
+        p.close
+    except FileNotFoundError:
+        sha256_result = str(generate_sha256(content))
+        num=str(int(next[0])+1)
+        n = open(start,'a') 
+        n.write("Sha256 of previous block: "+sha256_result)
+        n.write('\n')
+        n.write("Next block: "+num+".txt")
+        n.write('\n')
+        n.write(trade)
+        break
+
+    f = open(start) 
+    text = []
+    for line in f:
+        text.append(line)
+    if len(text) != 7:
+        n=open(start,'a')
+        n.write('\n')
+        n.write(trade)
+        n.close
+    next = text[1][12:-1]
+    start = "SHARE/" + next
+
