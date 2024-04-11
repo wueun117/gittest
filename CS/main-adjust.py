92f7f8f914bb8e3d531d6e139a1cb033a374826e
wueun <eason610617@gmail.com>
2024-04-11 09:56:58 +0800

    240411

--- CS/main-adjust.py
+++ CS/main-adjust.py
@@ -2,6 +2,7 @@ from Functions import transaction, checkLog, checkMoney, checkChain, checkAllCha
 
 import socket
 import threading
+import time
 
 class P2PNode:
     def __init__(self, port, peers):
@@ -10,7 +11,7 @@ class P2PNode:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
         # 設置 IPv4地址（AF_INET）和 UDP協議（SOCK_DGRAM）
         self.sock.bind(('127.0.0.1', self.port)) 
-        self.re = "0"
+        self.re = ["96dc773c9fd47e5377ccc9ce079f48e1de0922ffd22a2660fad39b8119789829"]
         # 設置此client節點IP
     
     def start(self, message=None):
@@ -26,17 +27,13 @@ class P2PNode:
             if len(mess) == 3:
                 transaction(data.decode('utf-8'))
                 # 一般的轉帳紀錄 # 做transaction 更新本地帳本
-                print("aaaaa")
             elif len(mess[0]) >= 20:
-                self.re = data.decode('utf-8')
-                print(self.re)
-                print("bbbbb")
+                self.re.append(data.decode('utf-8'))
             else:
                 self._send_messages(checkAllChains(data.decode('utf-8')))
                 # 檢查節點的指令
                 # 大家都做checkAllChains 回傳的sha拿來比較
                 # if一樣 再來做transaction
-                print("ccccc")
     
     def _send_messages(self, message):
         for peer in self.peers:
@@ -73,27 +70,24 @@ elif command == "checkChain":
         node.start("agale, " + who + ", 10")
 
 
-
-##############################################################
-
-
-
 #叫大家去做檢查
 elif command == "checkAllChains":
     who=input("Who are you: ")
     node.start("checkAllChains "+who)
     node.start()
-    print(node.re)
-    if print(checkAllChains(who)+"OUT") == node.re:
-        transaction("angle, " + who + ", 100")
-    print("end")
-   
+    time.sleep(5)
+    # print(node.re)
+    wrong=1000
+    for i in range(0,len(node.re)-1):
+        if str(checkAllChains(who)) != node.re[i]:
+            wrong=i
+            break
+    if wrong == 1000:
+        transaction("angle, " + who + ", 100")        
 else:
     print("Do not support")
 
 
 
 
-
-print(node.re)
 
